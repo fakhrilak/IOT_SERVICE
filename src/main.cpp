db3ee75c4ac48007e714f7699efa53a02de3dd70
risangadlih <risangadlih@gmail.com>
2021-08-08 16:59:06 +0700

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <SPI.h> 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include "accsessPoin.h"
@@ -5,10 +6,18 @@
 #include <SocketIoClient.h>
 #include <httpClient.h>
 #include <ArduinoJson.h>
+#include <MFRC522.h>
+
+#define RELAY 3
+#define SS_PIN 4
+#define RST_PIN 5
+MFRC522 mfrc522(SS_PIN, RST_PIN); 
+
 SocketIoClient Socket;
 StaticJsonDocument<200> doc;
 String token="-";
 String status = "0";
+
 void event(const char * payload, size_t length) {
   Serial.print("ini response socket : ");
   Serial.println(payload);
@@ -32,7 +41,7 @@ void event2(const char * payload, size_t length) {
 void setup() {
  // put your setup code here, to run once:
   Serial.begin(9600);
-  //  String data = "-";
+  // String data = "-";
 
   // Serial.print("Writing Data:");
   // Serial.println(data);
@@ -40,7 +49,7 @@ void setup() {
   // writeString(26,data);
   // writeString(51, data);
   // writeString(76,data);
-  delay(10);
+  // delay(10);
   String data1 = read_String(0);
   String data2 = read_String(26);
   String data3 = read_String(51);
@@ -77,18 +86,63 @@ void setup() {
         String change = "toChangeMicon-"+data3;
         Socket.on(status.c_str(), event);
         Socket.on(change.c_str(),event2);
-    }
-    
+        
+        SPI.begin();      
+        mfrc522.PCD_Init();
+        pinMode(RELAY, OUTPUT);   
+        digitalWrite(RELAY, HIGH);
+        Serial.println("Put your card to the reader...");
+        Serial.println();
+      } 
   }
 }
  
 void loop(){
    Socket.loop();
    if(status=="0"){
-     ///lock, gabisa ngapa ngapain
+     ///lock, gabisgia ngapa ngapain
    }else if(status == "1"){
      ///jika di tap maka add kunci
-   }else if(status == "2"){
-     ///jika di tap maka open kunci
    }
-}
+   else if(status == "2"){
+     ///jika di tap maka open kunci
+        if ( ! mfrc522.PICC_IsNewCardPresent()) 
+        {
+          return;
+        }
+        if ( ! mfrc522.PICC_ReadCardSerial()) 
+        {
+          return;
+        }
+        Serial.print("UID tag :");
+        String content= "";
+        byte letter;
+        for (byte i = 0; i < mfrc522.uid.size; i++) 
+        {
+          Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+          Serial.print(mfrc522.uid.uidByte[i], HEX);
+          content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+          content.concat(String(mfrc522.uid.uidByte[i], HEX));
+        }
+        Serial.println();
+        Serial.print("Message : ");
+        content.toUpperCase();
+        if (content.substring(1) == "XX XX XX XX")
+        {
+          Serial.println("Authorized access");
+          Serial.println();
+          delay(500);
+          digitalWrite(RELAY, LOW);
+          delay(2000);
+          digitalWrite(RELAY, HIGH);
+        }
+      
+      else   {
+          Serial.println(" Access denied");
+          delay(500);
+          digitalWrite(RELAY, HIGH);
+          delay(2000);
+          digitalWrite(RELAY, LOW);
+        }
+      }
+   }
